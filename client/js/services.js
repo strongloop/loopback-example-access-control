1422709d57474c40653801f8413ddae44af89d5b
Miroslav Bajtoš <miroslav@strongloop.com>
2014-01-03 13:35:39 +0100

    Refactor accessToken handling into User service
    Introduce a new service `LoopBack` that holds accessToken and can
    provide other LoopBack-related functionality in the future.

--- client/js/services.js
+++ client/js/services.js
@@ -1,26 +1,43 @@
 angular.module('starter.services', ['ngResource'])
-  .factory('User', ['$resource', function($resource) {
+  .factory('LoopBack', function() {
+    return { accessToken: null };
+  })
+  .factory('User', ['$q', '$resource', 'LoopBack', function($q,
+                                                            $resource,
+                                                            LoopBack) {
     return $resource('/api/users/:id', {id: '@id'}, {
       login: {
         method: 'POST',
-        url: '/api/users/login'
+        url: '/api/users/login',
+        interceptor: {
+          response: function(response) {
+            var loginResult = response.data;
+            LoopBack.accessToken = loginResult.id;
+            return response || $q.when(response);
+          }
+        }
       },
       logout: {
         method: 'POST',
-        url: '/api/users/logout?sid=:token',
-        params: {token: '@token'}
+        url: '/api/users/logout',
+        interceptor: {
+          response: function(response) {
+            LoopBack.accessToken = null;
+            return response || $q.when(response);
+          }
+        }
       }
     });
   }])
   .config(function ($httpProvider) {
     $httpProvider.interceptors.push('requestInterceptor');
   })
-  .factory('requestInterceptor', function ($q, $rootScope) {
+  .factory('requestInterceptor', function ($q, LoopBack) {
     return {
            'request': function (config) {
                 console.log('config', config);
-                if($rootScope.accessToken) {
-                  config.headers.authorization = $rootScope.accessToken;
+                if(LoopBack.accessToken) {
+                  config.headers.authorization = LoopBack.accessToken;
                 }
                 return config || $q.when(config);
             }
