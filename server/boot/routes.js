0f3655bd1b815e4ada0e6034f738e141f170d9be
Simon Ho <simon@strongloop.com>
2014-11-13 09:36:06 -0800

    Remove console.log

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -14,7 +14,6 @@ module.exports = function(app) {
   router.post('/projects', function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
-    console.log(email, password);
     app.models.User.login({
       email: email,
       password: password
