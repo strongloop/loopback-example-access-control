a467c184b9bdba108354b089f46be78e2aa8b89d
crandmck <rand@strongloop.com>
2015-09-24 16:04:15 -0700

    Upgrade LB Explorer and related changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ var path = require('path');
 
 var app = module.exports = loopback();
 
-
 app.middleware('initial', bodyParser.urlencoded({ extended: true }));
 
 // Bootstrap the application, configure models, datasources and middleware.
@@ -23,7 +22,12 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
 };
 
