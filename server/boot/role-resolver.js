e28c7cf2e579805a838ad6cafdac7ab58337a0df
jannyHou <juehou@ca.ibm.com>
2017-08-24 11:06:21 -0400

    listen on connected

--- server/boot/role-resolver.js
+++ server/boot/role-resolver.js
@@ -7,46 +7,48 @@ module.exports = function(app, done) {
   var Role = app.models.Role;
   var db = app.datasources.mycloudant;
 
-  db.automigrate(function(err) {
-    if (err) return done(err);
-    Role.registerResolver('teamMember', function(role, context, cb) {
-      function reject() {
-        process.nextTick(function() {
-          cb(null, false);
-        });
-      }
-
-      // if the target model is not project
-      if (context.modelName !== 'project') {
-        return reject();
-      }
-
-      // do not allow anonymous users
-      var userId = context.accessToken.userId;
-      if (!userId) {
-        return reject();
-      }
-
-      // check if userId is in team table for the given project id
-      context.model.findById(context.modelId, function(err, project) {
-        if (err || !project)
-          return reject();
-
-        var Team = app.models.Team;
-        Team.count({
-          ownerId: project.ownerId,
-          memberId: userId
-        }, function(err, count) {
-          if (err) {
-            console.log(err);
+  db.once('connected', function(err) {
+    db.automigrate(function(err) {
+      if (err) return done(err);
+      Role.registerResolver('teamMember', function(role, context, cb) {
+        function reject() {
+          process.nextTick(function() {
             cb(null, false);
-          }
+          });
+        }
 
-          cb(null, count > 0); // true = is a team member
+        // if the target model is not project
+        if (context.modelName !== 'project') {
+          return reject();
+        }
+
+        // do not allow anonymous users
+        var userId = context.accessToken.userId;
+        if (!userId) {
+          return reject();
+        }
+
+        // check if userId is in team table for the given project id
+        context.model.findById(context.modelId, function(err, project) {
+          if (err || !project)
+            return reject();
+
+          var Team = app.models.Team;
+          Team.count({
+            ownerId: project.ownerId,
+            memberId: userId
+          }, function(err, count) {
+            if (err) {
+              console.log(err);
+              cb(null, false);
+            }
+
+            cb(null, count > 0); // true = is a team member
+          });
         });
       });
+      done();
     });
-    done();
   });
   
 };
